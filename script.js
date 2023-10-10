affa76fc3fa80f9b912f2c71924528bef93a804d
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-10-10 18:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if (typeof key !== 'string') {
+    return false; // Ensure the input is a valid string
+  }
+
+  return key in sampleObject;
 }
 
 // Do not change the code below
